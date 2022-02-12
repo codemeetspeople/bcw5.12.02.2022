b50974ff9c12be8e114c81c95bd2ed2a5313d18c
caiman <me@caiman.dev>
2022-02-12 18:36:54 +0200

    [master] string format

---
+++ src/loops.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    # array = [1, 2, 3, 4, 5]
+    #
+    # for x in array:
+    #     print(x)
+
+    # for x in range(1, 10, 2):
+    #     print(x)
+
+    # x = 1
+    #
+    # while x <= 5:
+    #     print(x)
+    #     x += 1
+
+    # params = {
+    #     'user': 'caiman',
+    #     'password': 'qwerty',
+    #     'role': 'trainer'
+    # }
+    #
+    # for key, value in params.items():
+    #     print(key, value)
+
+    for symbol in 'Hello':
+        print(symbol)
---
+++ src/strings.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    # x, y = list(map(int, input().strip().split()))
+    #
+    # print('%d + %d = %d' % (x, y, x+y))
+    # print('{} - {} = {}'.format(x, y, x-y))
+    # print('{x} * {y} = {result}'.format(x=x, y=y, result=x*y))
+    # print(f'{x} / {y} = {x / y}')
+
+    params = {
+        'male': 'John',
+        'female': 'Jane'
+    }
+
+    print('%(male)s + %(female)s = love' % params)
+    print('{male} + {female} = love'.format(**params))
+    print(f'{params["male"]} + {params["female"]} = love')
