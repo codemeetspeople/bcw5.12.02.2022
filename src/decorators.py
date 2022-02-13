4cdb7ba46e3852678b37fd67994136e776434362
caiman <me@caiman.dev>
2022-02-13 14:07:57 +0200

    [master] decorators

---
+++ src/decorators.py
@@ -0,0 +1,21 @@
+def run_N_times(times):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for i in range(times):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@run_N_times(2)
+def hello(username):
+    print(f'Hello, {username}!')
+
+
+@run_N_times(2)
+def hello2(username1, username2):
+    print(f'Hello, {username1} and {username2}!')
+
+
+if __name__ == '__main__':
+    hello2('caiman', 'saruman')
