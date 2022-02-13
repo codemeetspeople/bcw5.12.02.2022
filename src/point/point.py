b8f8d7e8bcb588e87fa427dfa5970315d8131a6c
caiman <me@caiman.dev>
2022-02-13 15:01:11 +0200

    [master] OOP

---
+++ src/point/point.py
@@ -0,0 +1,43 @@
+class Point:
+    # __slots__ = ('_x', '_y')
+
+    def __init__(self, x=0.0, y=0.0):
+        self._x = float(x)
+        self._y = float(y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not a == b
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @x.setter
+    def x(self, value):
+        self._x = float(value)
+
+    @y.setter
+    def y(self, value):
+        self._y = float(value)
+
+
+if __name__ == '__main__':
+    a = Point()
+    b = Point()
+
+    if a == b:
+        print(f'{a} == {b}')
+    else:
+        print(f'{a} != {b}')
